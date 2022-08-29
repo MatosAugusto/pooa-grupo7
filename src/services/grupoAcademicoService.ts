ac4ba7cd724113f1a0c8c1b5a82665f102f0b7c5
MatosAugusto <augustomatos@estudante.ufscar.br>
2022-08-29 19:06:11 -0300

    integracao

--- src/services/grupoAcademicoService.ts
+++ src/services/grupoAcademicoService.ts
@@ -1,4 +1,11 @@
+import { Usuario } from "../classes/usuario";
+import { Perfil } from "../classes/perfil";
 import { repositoryGrupoAcademico } from "../repositorio/repositoryGrupoAcademico";
 export class grupoacademicoService{
     private grupoAcadRepository = new repositoryGrupoAcademico();
+    async getById(idGrupo: string, user : Usuario){
+        if (!user.getPerfil.podeExecutar(desativarGrupo)) {
+            throw new Error("Você não pode acessar esse resource");
+          }
+    }
 }
